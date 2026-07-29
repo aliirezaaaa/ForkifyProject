1a47bf20d804d1c51d14adc87b296d3310270c83
Alireza <alireza.khorramshekooh@gmail.com>
2026-07-29 13:05:03 +0330

    feat listening for load and hashchange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,9 +28,11 @@ const renderSpinner = function (parentEl) {
 };
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -140,4 +142,5 @@ const showRecipe = async function () {
     alert(err);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
