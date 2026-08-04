1ca5d94f3e530b986e4a224f56534363fa2b7bf6
Alireza <alireza.khorramshekooh@gmail.com>
2026-08-04 17:12:14 +0330

    modified to extends view class

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,58 +1,10 @@
+import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 
-class RecipeView {
+class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = 'We could not find this recipe! Please try another.';
   _message = '';
-  _data;
-
-  render(data) {
-    this._data = data;
-    const markup = this._generateMarkUp();
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  _clear() {
-    this._parentElement.innerHTML = '';
-  }
-
-  renderSpinner = function () {
-    const markup = `
-      <div class="spinner">
-        <svg>
-          <use href="${icons}#icon-loader"></use>
-        </svg>
-      </div>`;
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  };
-
-  renderError(message = this._errorMessage) {
-    const markup = `<div class="error">
-        <div>
-          <svg>
-            <use href="${icons}#icon-alert-triangle"></use>
-          </svg>
-        </div>
-        <p>${message}</p>
-    </div>`;
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  renderMessage(message = this._message) {
-    const markup = `<div class="message">
-        <div>
-          <svg>
-            <use href="${icons}#icon-smile"></use>
-          </svg>
-        </div>
-        <p>${message}</p>
-    </div>`;
-    this._clear();
-    this._parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
